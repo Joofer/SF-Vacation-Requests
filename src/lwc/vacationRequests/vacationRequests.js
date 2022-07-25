ff4efa94549c429aa711b0cc685f7eb507dea1b9
Andrey Loskutov <joofer.sp@gmail.com>
2022-07-26 00:30:03 +0300

    Changed data display type to table

--- src/lwc/vacationRequests/vacationRequests.js
+++ src/lwc/vacationRequests/vacationRequests.js
@@ -31,6 +31,7 @@ export default class VacationRequests extends LightningElement {
 
     handleSuccess(event) {
         this.isSucceed = true;
+        this.handleLoad();
     }
 
     connectedCallback() {
