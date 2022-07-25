05968e226d19bbe043e855bcf47305f3b6f85dfd
Andrey Loskutov <joofer.sp@gmail.com>
2022-07-25 23:53:44 +0300

    Form prototype test

--- src/lwc/vacationRequests/vacationRequests.js
+++ src/lwc/vacationRequests/vacationRequests.js
@@ -3,6 +3,7 @@ import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 import getVacationRequestList from '@salesforce/apex/VacationRequestsController.getVacationRequestList';
 
 import REQUEST_OBJECT from '@salesforce/schema/Vacation_Request__c';
+import REQUEST_REQUESTTYPE_FIELD from '@salesforce/schema/Vacation_Request__c.RequestType__c';
 import REQUEST_STARTDATE_FIELD from '@salesforce/schema/Vacation_Request__c.StartDate__c';
 import REQUEST_ENDDATE_FIELD from '@salesforce/schema/Vacation_Request__c.EndDate__c';
 import REQUEST_WORKINGDAYS_FIELD from '@salesforce/schema/Vacation_Request__c.WorkingDays__c';
@@ -16,7 +17,7 @@ export default class VacationRequests extends LightningElement {
     @track isModalAddRequestShown = false;
 
     objectApiName = REQUEST_OBJECT;
-    fields = [REQUEST_STARTDATE_FIELD, REQUEST_ENDDATE_FIELD, REQUEST_WORKINGDAYS_FIELD, REQUEST_MANAGER_FIELD, REQUEST_STATUS_FIELD];
+    fields = [REQUEST_REQUESTTYPE_FIELD, REQUEST_STARTDATE_FIELD, REQUEST_ENDDATE_FIELD, REQUEST_WORKINGDAYS_FIELD, REQUEST_MANAGER_FIELD, REQUEST_STATUS_FIELD];
 
     handleLoad() {
         getVacationRequestList()
